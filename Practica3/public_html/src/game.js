050c3eb6f6157cad96c96f58040f70d4dd7c49e2
Jose Luis <joseluissanchez11@gmail.com>
2018-04-05 19:17:57 +0200

    Canvas funcional con quintus

--- Practica3/public_html/src/game.js
+++ Practica3/public_html/src/game.js
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var game =function() {
+var Game =function() {
     var Q = window.Q = Quintus({ development:true})
     .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")//Librerias del quintus cargadas
-    .setup({ maximize:true})// Maximize this game to whatever the size of the browser is
-    .controls().touch();// Turn on default input controls and touch input (for UI)
+    .setup({ maximize: "touch" });// Maximize this game to mobiles
+    
     
     //Precarga de elementos
     Q.preload([]);
@@ -16,4 +16,16 @@ var game =function() {
         
     });
 };
+/*
+ * Genera un numero aleatorio entre los pasados por parametros
+ */
+Quintus.Random = function(Q) {
+Q.random = function(min,max) {
+    return min + Math.random() * (max - min);
+    };
+};
 
+//Evento de inicio del juego
+window.addEventListener("load", function() {
+    Game();
+});
