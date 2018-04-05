b8785ba8708481f6bbf8de2e99d78874deb82573
usuario_local <usuario_local@PTO0107>
2018-04-05 15:46:16 +0200

    Cosas basicas

--- Practica3/public_html/src/game.js
+++ Practica3/public_html/src/game.js
@@ -3,5 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+var game =function() {
+    var Q = window.Q = Quintus({ development:true})
+    .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")//Librerias del quintus cargadas
+    .setup({ maximize:true})// Maximize this game to whatever the size of the browser is
+    .controls().touch();// Turn on default input controls and touch input (for UI)
+    
+    //Precarga de elementos
+    Q.preload([]);
+    //Inicio del juego
+    Q.load([],function(){
+        
+    });
+};
 
