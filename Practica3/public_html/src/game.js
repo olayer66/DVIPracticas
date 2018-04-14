ed2016315d17afcb9b96a73d1b7916d8046ea52a
Jose Luis <joseluissanchez11@gmail.com>
2018-04-14 14:02:52 +0200

    Mario muere si le tocan o se cae Pantalla de endgame aiBounce(coportamientyo basico enemigos)

--- Practica3/public_html/src/game.js
+++ Practica3/public_html/src/game.js
@@ -1,4 +1,7 @@
 /*---------------------------CARGA DE QUINTUS---------------------------------*/
+var SPRITE_PLAYER = 1;
+var SPRITE_TILES = 2;
+var SPRITE_ENEMY = 4;
 /* global Quintus */
 var Q = window.Q = Quintus({ development:true,audioSupported: ['ogg','mp3'] })
                 .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI,TMX,Audio")//Librerias del quintus cargadas
@@ -27,18 +30,33 @@ Q.preload(function(){
     Q.compileSheets("bloopa.png","bloopa.json");
     Q.compileSheets("goomba.png","goomba.json");
     Q.loadTMX("level.tmx", function() {
-        Q.stageScene("level1");
+    Q.stageScene("level1");
     });
 });
 
+/*-----------------------------ANIMACIONES----------------------------------*/
+
+Q.animations('Mario', {
+  run_right: { frames: [0,1,2], rate: 1/5}, 
+  run_left: { frames: [15,16,17], rate:1/5 },
+  fire_right: { frames: [9,10,10], next: 'stand_right', rate: 1/30, trigger: "fired" },
+  fire_left: { frames: [20,21,21], next: 'stand_left', rate: 1/30, trigger: "fired" },
+  stand_right: { frames: [0], rate: 1 },
+  stand_left: { frames: [15], rate: 1},
+  fall_right: { frames: [2], loop: false },
+  fall_left: { frames: [14], loop: false }
+});
 /*-----------------------------ENEMIGOS----------------------------------*/
+
 
 Q.Sprite.extend("Bloopa",{ 
     init: function(p) { 
         this._super(p, { 
             sheet: "bloopa",
             frame: 0,
-            vx:100
+            vx:100,
+            type: SPRITE_ENEMY,
+            collisionMask: SPRITE_PLAYER | SPRITE_TILES
         }); 
         this.add("2d,aiBounce");
     }
@@ -51,9 +69,11 @@ Q.Sprite.extend("Goomba",{
             damage: 5, 
             x: 5, 
             y: 1,
+            vx:100,
             sheet: "goomba",
             frame: 0,
-            vx:100
+            type: SPRITE_ENEMY,
+            collisionMask: SPRITE_PLAYER | SPRITE_TILES
         }); 
         this.add("2d,aiBounce");        
     }
@@ -68,7 +88,8 @@ Q.Sprite.extend("Piranha",{
             y: 1,
             sheet: "piranha",
             frame: 0,
-            vy:100
+            type: SPRITE_ENEMY,
+            collisionMask: SPRITE_PLAYER | SPRITE_TILES | SPRITE_ENEMY
         }); 
         this.add("2d,aiBounce");        
     }
@@ -82,7 +103,9 @@ Q.Sprite.extend("Mario",{
     init:function(p) {
         this._super(p, {
             sheet:"marioR",
-            frame:0
+            sprite:"Mario",
+            frame:0,
+            lifes:1
         });
         this.add("2d,platformerControls,animation");
         this.on("bump.bottom",this,"stompB");
@@ -92,29 +115,23 @@ Q.Sprite.extend("Mario",{
     },stompB:function(collision) {
         salto=false;
         Q.audio.stop("jump_small.ogg");
-        if(collision.obj.isA("Bloopa")) {
+        if(collision.obj.p.type===SPRITE_ENEMY) {
            collision.obj.destroy();        
            Q.audio.play('kill_enemy.ogg');
            this.p.vy = -300;// make the player jump
         }
     },
     stompR:function(collision) {
-        if(collision.obj.isA("Bloopa")) {
-            collision.obj.destroy();
-            this.muerte();
-        }     
+        if(collision.obj.p.type===SPRITE_ENEMY) 
+            this.muerte();    
     },
     stompL:function(collision) {
-        if(collision.obj.isA("Bloopa")) {
-            collision.obj.destroy();
+        if(collision.obj.p.type===SPRITE_ENEMY) 
             this.muerte();
-        } 
     },
     stompT:function(collision) {
-        if(collision.obj.isA("Bloopa")) {
-            collision.obj.destroy();
+        if(collision.obj.p.type===SPRITE_ENEMY) 
             this.muerte();
-        } 
     },
     step:function(){
         if(Q.inputs['up'] && salto===false) {//salto
@@ -122,6 +139,16 @@ Q.Sprite.extend("Mario",{
             salto=true;
             Q.audio.play('jump_small.ogg');
         }
+        
+        if(this.p.vx > 0) {
+            this.play("run_right");
+        } else if(this.p.vx < 0) {
+            this.play("run_left");
+        } else {
+            if(this.p.direction ==="right")this.play("stand_right");
+            else this.play("stand_left");
+        }
+
         if(!Q.inputs['up']){
             this.p.gravity=1;
         }
@@ -141,7 +168,7 @@ Q.Sprite.extend("Mario",{
 Q.scene("level1",function(stage) {
     var mario= new Q.Mario({x:630,y:400});
     var b= new Q.Bloopa({x:730,y:400});
-    var a= new Q.Goomba();
+    var a= new Q.Goomba({x:780,y:450});
     var c = new Q.Piranha();
     Q.stageTMX("level.tmx",stage);
     //Q.audio.play('music_main.ogg',{ loop:true});
@@ -149,7 +176,7 @@ Q.scene("level1",function(stage) {
     stage.insert(b);
     stage.add("viewport").follow(mario,{x:true,y:false});
     stage.viewport.offsetX=150;
-    //stage.insert(a);
+    stage.insert(a);
     //stage.insert(c);
     
 });
